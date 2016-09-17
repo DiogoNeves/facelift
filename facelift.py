1cb3062b1858a1c94e5a30b613d6dacf4ea2d346
Diogo Neves <diogo.neves@gmail.com>
2016-09-17 18:43:31 +0100

    Created simple test and face detection script (broken)

--- facelift.py
+++ facelift.py
@@ -1,2 +1,38 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# noinspection PyUnresolvedReferences,PyPackageRequirements
+import cv2
+
+
+def load_image(image_path):
+    """
+    Load image from image file path. Currently serves as a wrapper for
+    OpenCV library.
+    :param image_path: Valid path to image.
+    :return: Image object.
+    """
+    assert image_path
+    return cv2.imread(image_path)
+
+
+def get_faces_in(image):
+    """
+    Detect all faces in the image.
+    :param image: Image with faces to detect.
+    :return: Tuple of face region rectangles, empty if none detected.
+    """
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    return face_cascade.detectMultiScale(gray, 1.2, 5)
+
+
+if __name__ == '__main__':
+    img = load_image('test_resources/test_face.jpg')
+    fcs = get_faces_in(img)
+    for (x, y, w, h) in fcs:
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- test_facelift.py
+++ test_facelift.py
@@ -1,9 +1,11 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
+from facelift import load_image, get_faces_in
 
-import pytest
+TEST_IMAGE_PATH = 'test_resources/test_face.jpg'
 
 
-class TestCase(object):
-    def test_something(self):
-        assert False
+def test_it_detects_a_face():
+    image = load_image(TEST_IMAGE_PATH)
+    faces = get_faces_in(image)
+    assert len(faces) == 1
